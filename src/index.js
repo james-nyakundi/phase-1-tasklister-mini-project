c561daf9a7dbe08dad789787982fa39cb4644889
james nyakundi <omarejames082@gmail.com>
2024-04-16 00:26:41 +0300

    Adds  src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskInput = document.getElementById('taskInput');
+const submitButton = document.getElementById('submitTask');
+const taskList = document.getElementById('taskList');
+
+submitButton.addEventListener('click', function() {
+    const task = taskInput.value;
+    const taskItem = document.createElement('li');
+    taskItem.textContent = task;
+    taskList.appendChild(taskItem);
+    taskInput.value = '';
+});
 });
